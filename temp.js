05fd0fba2389cbbd63187df32883d41d814467cb
bagashinji <1059868558@qq.com>
2022-11-26 13:44:13 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  test
+// @author       bagashinji
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log('hello world');
+})();
